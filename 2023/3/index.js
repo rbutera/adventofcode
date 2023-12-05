9dd76a98d9a7fb718d8646552ae0879004b97aa3
Rai Butera <rai@rbutera.com>
2023-12-05 19:14:40 +0000

    making progress on day 3

--- 2023/3/index.js
+++ 2023/3/index.js
@@ -18,58 +18,158 @@ function toGrid(input) {
   return input.split("\n").map((x) => x.split(""));
 }
 
-function toPartNumber(x, y, grid) {
+function getRowAndValue(x, y, grid) {
   const row = grid[y];
   const value = row[x];
+  const hasRowBelow = y !== grid.length - 1;
+  const hasRowAbove = y !== 0;
+  const previous = hasRowAbove && grid[y - 1];
+  const next = hasRowBelow && grid[y + 1];
+
+  return { row, value, previous, next };
+}
+
+function getCurrentNumber(x, y, grid) {
+  const { row, value } = getRowAndValue(x, y, grid);
+
+  let buffer = [value];
+
+  const shouldSearchLeft = x > 0;
+  const shouldSearchRight = x < row.length - 1;
+
+  const boundaries = {
+    start: 0,
+    end: row.length - 1,
+  };
+
+  if (shouldSearchLeft) {
+    let cursor = x - 1;
+    while (cursor >= 0) {
+      let current = row[cursor];
+      if (/\d/.test(current)) {
+        buffer.unshift(current);
+        boundaries.start = cursor;
+      } else {
+        break;
+      }
+      cursor--;
+    }
+  }
+
+  if (shouldSearchRight) {
+    let cursor = x + 1;
+    while (cursor < row.length) {
+      let current = row[cursor];
+      if (/\d/.test(current)) {
+        buffer.push(current);
+        boundaries.end = cursor;
+      } else {
+        break;
+      }
+      cursor++;
+    }
+  }
+
+  return {
+    part: Number.parseInt(buffer.join(""), 10),
+    y,
+    boundaries,
+  };
+}
+
+const searchLeftAndRight = (inputRow, inputX, previous, next) => {
+  // check left and right
+  if (inputX != 0) {
+    // check left
+    if (isSymbol(inputRow[inputX - 1])) {
+      return true;
+    }
+  }
+
+  if (inputX < inputRow.length - 1) {
+    // check right
+    if (isSymbol(inputRow[inputX + 1])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const searchUpAndDown = (inputRow, inputX, previous, next) => {
+  if (previous) {
+    // check up
+    if (isSymbol(previous[inputX])) {
+      return true;
+    }
+    // check up-left
+    if (inputX > 0) {
+      if (isSymbol(previous[inputX - 1])) {
+        return true;
+      }
+    }
+    if (inputX < inputRow.length - 1) {
+      if (isSymbol(previous[inputX + 1])) {
+        return true;
+      }
+    }
+  }
+
+  if (next) {
+    // check down p
+    if (isSymbol(next[inputX])) {
+      return true;
+    }
+
+    // check down-left
+    if (inputX > 0) {
+      if (isSymbol(next[inputX - 1])) {
+        return true;
+      }
+    }
+
+    // check down-right
+    if (inputX < inputRow.length - 1) {
+      if (isSymbol(next[inputX + 1])) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
+
+function toPartNumber(x, y, grid) {
+  const { row, value, previous, next } = getRowAndValue(x, y, grid);
 
   if (!value.match(/\d/)) {
     return 0;
   }
 
-  const hasRowBelow = y !== grid.length - 1;
-  const hasRowAbove = y !== 0;
-  const previousRow = hasRowAbove && grid[y - 1];
-  const nextRow = hasRowBelow && grid[y + 1];
-
   const xMax = row.length - 1;
 
-  // part is a number, check for adjacent
+  // get current number and bounds
+  const { part, boundaries } = getCurrentNumber(x, y, grid);
+  console.log(
+    `(${x}, ${y}) => current number is ${part}. it starts at x = ${boundaries.start} and ends at x = ${boundaries.end}`
+  );
 
-  const searchLeftAndRight = (inputRow, inputX) => {
-    // check left and right
-    if (inputX != 0) {
-      // check left
-      if (isSymbol(row[inputX - 1])) {
-        return value;
-      }
-    }
+  let found = false;
+  let cursor = boundaries.start;
 
-    if (inputX <= xMax) {
-      // check right
-      if (isSymbol(row[inputX + 1])) {
-        return value;
-      }
-    }
-    return false;
-  };
-
-  if (hasRowAbove) {
-    // check up
-    // check up-left
-    if (x <= xMax) {
-      // check up-right
-    }
-  }
-
-  if (hasRowBelow) {
-    // check down
-    // check down-left
-    if (x <= xMax) {
-      // check down-right
+  while (!found && cursor <= xMax) {
+    found = searchLeftAndRight || searchUpAndDown;
+    if (found && DEBUG_LOG) {
+      console.log(`found a symbol!`);
+      return {
+        part,
+        boundaries,
+      };
     }
   }
 }
 
-function main() {}
+function main() {
+  console.log(toPartNumber(2, 3, toGrid(sources.training)));
+}
 
 main();
